791bccce7d9208d1a875ebb5ab9cd9df0ab68544
cfop2357 <yoguel.salazar@gmail.com>
2021-03-31 21:00:40 -0600

    codeforces EDU/Segment Tree 1/step 3/A

---
+++ Segment Tree 1/step 3/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Tree {
+	typedef int T;
+	static constexpr T unit = 0;
+	T f(T a, T b) { return a + b; } // (any associative fn)
+	vector<T> s; int n;
+	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
+	void update(int pos, T val) {
+		for (s[pos += n] = val; pos /= 2;)
+			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+	}
+	T query(int b, int e) { // query [b, e)
+		T ra = unit, rb = unit;
+		for (b += n, e += n; b < e; b /= 2, e /= 2) {
+			if (b % 2) ra = f(ra, s[b++]);
+			if (e % 2) rb = f(s[--e], rb);
+		}
+		return f(ra, rb);
+	}
+};
+
+int main(){
+    int n; cin>>n;
+    vector<int> P(n);
+    for(int &p : P)
+        cin>>p;
+
+    Tree st(n+1);
+    for(int p : P){
+        cout<<st.query(p, n+1)<<" ";
+        st.update(p, 1);
+    }
+    cout<<"\n";
+}
